12bb2f09497af870c2693f8286065725ce83ab31
InvalidCS <cjdipalma@yahoo.com>
2018-10-25 18:14:17 -0700

    Short Range attacks, first iteration

---
+++ SOADsavesMetal/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	// Short range attack
+    private GameObject shortRangeHitbox;
+    private bool attacking;
+    private const float SHORT_ATTACK_HOLD_DURATION = 0.3f;
+    private float currAttackTime = 0.0f;
+    private const float SHORT_ATTACK_COOLDOWN = 0.2f;
+    private float currCooldown = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        shortRangeHitbox = GameObject.Find("Player/ShortRange");
+        print(shortRangeHitbox);
+        shortRangeHitbox.SetActive(false);
+        attacking = false;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if(!attacking && Input.GetKey(KeyCode.Z))
+		{
+			Debug.Log("z was pressed");
+			StartCoroutine(AttackShort());
+		}
+	}
+
+    IEnumerator AttackShort()
+    {
+        attacking = true;
+        shortRangeHitbox.SetActive(true);
+        currAttackTime = 0.0f;
+        while(currAttackTime < SHORT_ATTACK_HOLD_DURATION)
+        {
+        	currAttackTime += Time.deltaTime;
+        	yield return null;
+        }
+        shortRangeHitbox.SetActive(false);
+
+        // Cooldown period
+        currCooldown = 0.0f;
+        while(currCooldown < SHORT_ATTACK_COOLDOWN)
+        {
+        	currCooldown += Time.deltaTime;
+        	yield return null;
+        }
+
+        attacking = false;
+    }
+
+
+}
---
+++ SOADsavesMetal/Assets/Scripts/SimpleWalk.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleWalk : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private float timer;
+	private const float BASE_VELOCITY = 1.5f;
+	private const float TIMER_MAX = 1.4f;
+	private bool hasBeenHit = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		timer = 0.0f;
+		rb.velocity = new Vector2(-BASE_VELOCITY, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		timer += Time.deltaTime;
+		if(timer > TIMER_MAX)
+		{
+			timer = 0.0f;
+			rb.velocity = new Vector2(-1.0f * rb.velocity.x, 0.0f);
+		}
+	}
+
+	void hit()
+	{	
+		if(!hasBeenHit)
+		{
+			hasBeenHit = true;
+			print("I've been hit");
+		}
+	}
+}
