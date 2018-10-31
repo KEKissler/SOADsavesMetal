33cb07cc2d3b972b275f51181a5f1502ce739c46
InvalidCS <cjdipalma@yahoo.com>
2018-10-31 13:53:43 -0700

    Long range attack partial implementation

--- SOADsavesMetal/Assets/Scripts/PlayerAttack.cs
+++ SOADsavesMetal/Assets/Scripts/PlayerAttack.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
+	
+	private bool attacking;
+	private float currCooldown = 0.0f;
+	private float currAttackTime = 0.0f;
 
 	// Short range attack
     private GameObject shortRangeHitbox;
-    private bool attacking;
     private const float SHORT_ATTACK_HOLD_DURATION = 0.3f;
-    private float currAttackTime = 0.0f;
     private const float SHORT_ATTACK_COOLDOWN = 0.2f;
-    private float currCooldown = 0.0f;
-
+    
+    // Long range attack
+    private GameObject drumstick;
+    private const float LONG_ATTACK_COOLDOWN = 0.35f;
+    
 	// Use this for initialization
 	void Start () {
         shortRangeHitbox = GameObject.Find("Player/ShortRange");
@@ -23,10 +28,18 @@ public class PlayerAttack : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		if(!attacking && Input.GetKey(KeyCode.Z))
+		if(!attacking)
 		{
-			Debug.Log("z was pressed");
-			StartCoroutine(AttackShort());
+			if(Input.GetKey(KeyCode.Z))
+			{
+				Debug.Log("z was pressed");
+				StartCoroutine(AttackShort());
+			}
+			else if(Input.GetKey(KeyCode.X))
+			{
+				Debug.Log("x was pressed");
+				StartCoroutine(AttackLong());
+			}
 		}
 	}
 
@@ -53,5 +66,23 @@ public class PlayerAttack : MonoBehaviour {
         attacking = false;
     }
 
+    IEnumerator AttackLong()
+    {
+    	attacking = true;
+
+    	// Create projectile (drumstick)
+
+        // Cooldown period
+        currCooldown = 0.0f;
+        while(currCooldown < LONG_ATTACK_COOLDOWN)
+        {
+        	currCooldown += Time.deltaTime;
+        	yield return null;
+        }
+
+    	attacking = false;
+    }
+
+    // Create variable get methods here
 
 }
