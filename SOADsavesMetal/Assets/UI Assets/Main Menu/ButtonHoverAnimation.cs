7086de706d21c709154e341d82e0ad73fe0cce48
Victoria B <vbarinova97@gmail.com>
2018-10-31 15:00:13 -0700

    Menu Buttons Basic Test
    Creepy eyes hover over placeholder buttons. script works. Shader stuff
    in progress

---
+++ SOADsavesMetal/Assets/UI Assets/Main Menu/ButtonHoverAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonHoverAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public GameObject left_eye;
+    public GameObject right_eye;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        left_eye.SetActive(true);
+        right_eye.SetActive(true);
+        left_eye.transform.position = new Vector3(left_eye.transform.position.x, transform.position.y, 0);
+        right_eye.transform.position = new Vector3(right_eye.transform.position.x, transform.position.y, 0);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //if (eventData.pointerCurrentRaycast.gameObject.name == "Button") { return;}
+        left_eye.SetActive(false);
+        right_eye.SetActive(false);
+    }
+
+}
