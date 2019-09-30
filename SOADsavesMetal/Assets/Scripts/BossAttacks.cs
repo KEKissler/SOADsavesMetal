d2fb17c34817bbafbc6f1bbecd567b02efff0140
Keegan Frederick <keegandfrederick@gmail.com>
2019-09-30 00:00:06 -0700

    Added first scriptable object BossAttacks and added it to create under Attack. Started working on ElectricShock scriptable attack.

---
+++ SOADsavesMetal/Assets/Scripts/BossAttacks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Attack", menuName = "Attack")]
+public class BossAttacks : ScriptableObject
+{
+    
+
+    public string name;
+    public string description;
+
+    public Animation animation;
+
+    public ProjectileSpeed projectileSpeed;
+    //public int speed;
+    public int damage;
+    public int duration;
+
+
+}
