44305cbf0ead2696c96509a1cb3ded90f531d5ba
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-10-14 14:44:17 -0700

    Create Agas Managing Scripts

---
+++ SOADsavesMetal/Assets/Scripts/BossAttackManagers/AgasAttackManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class AgasAttackManager : BossAttackManager<AgasPhase>
+{
+    protected override AttackOptions GetNextOptions(AgasPhase phase)
+    {
+        return phase.SelectNextAttackOption();
+    }
+
+    protected override AgasPhase GetNextPhase(List<PhaseChangeThreshhold> phaseChangeThreshhold, int phaseIndex)
+    {
+        return (AgasPhase)phaseChangeThreshhold[phaseIndex].BossPhase;
+    }
+}
--- SOADsavesMetal/Assets/Scripts/BossAttackManagers/TsovinarAttackManager.cs
+++ SOADsavesMetal/Assets/Scripts/BossAttackManagers/TsovinarAttackManager.cs
@@ -1,69 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
+﻿using System.Collections.Generic;
 
-public class TsovinarAttackManager : BossAttackManager
+public class TsovinarAttackManager : BossAttackManager<TsovinarPhase>
 {
-    //all prior attack state
-    private BossAttack attack;
-    private AttackOptions options;
-    private TsovinarPhase phase;
-    private int phaseIndex;
-
-    private Coroutine attackManager;
-
-    void Start()
+    protected override AttackOptions GetNextOptions(TsovinarPhase phase)
     {
-        if(PhaseChangeThreshholds.Count < 1)
-        {
-            Debug.LogWarning("TsovinarAttackManager not setup.");
-            return;
-        }
-        phase = (TsovinarPhase)PhaseChangeThreshholds[0].BossPhase;
-        if (phaseIndex < PhaseChangeThreshholds.Count - 1)
-        {
-            phaseIndex++;
-        }
-        attackManager = StartCoroutine(ManageAttacks());
+        return phase.SelectNextAttackOption();
     }
 
-    private IEnumerator ManageAttacks()
+    protected override TsovinarPhase GetNextPhase(List<PhaseChangeThreshhold> phaseChangeThreshhold, int phaseIndex)
     {
-        yield return new WaitForEndOfFrame();
-        while (true)
-        {
-            attack = SelectNextAttack();
-            attack.ExecuteAttack();
-            yield return new WaitForSeconds(attack.duration);
-        }
-    }
-
-    private BossAttack SelectNextAttack()
-    {
-        //if new phase condition reached, switch phase (or announce end of final phase)
-        if (BossHealth.getHPPercentage() < PhaseChangeThreshholds[phaseIndex].HealthPercentThreshhold)
-        {
-            if (phaseIndex < PhaseChangeThreshholds.Count - 1)
-            {
-                phaseIndex++;
-            }
-            else
-            {
-                FinalPhaseEnded();
-            }
-            phase = (TsovinarPhase)PhaseChangeThreshholds[phaseIndex].BossPhase;
-        }
-        //get current attack options from phase
-        options = phase.SelectNextAttackOption();
-        //get current attack from options
-        return options.GetNextAttack();
-    }
-
-    private void FinalPhaseEnded()
-    {
-        StopCoroutine(attackManager);
-        attackManager = null;
-        Debug.Log("Ran Out of Phases, could not get next attack.");
+        return (TsovinarPhase)phaseChangeThreshhold[phaseIndex].BossPhase;
     }
 }
