0af3f857ebf203fe7b3976f8a889714395b0db94
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-11-14 12:28:58 -0800

    Add Overflow attack w/o animation sprite

---
+++ SOADsavesMetal/Assets/Scripts/BossAttacks/Boss1/Overflow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Attack/Agas/Overflow")]
+
+public class Overflow : AgasAttack
+{
+    public float floodTime;
+
+    private DecayOverflow overflowLiquid;
+
+    public override void Initialize(AgasAttackData data)
+    {
+        overflowLiquid = data.DecayOverflow;
+    }
+
+    protected override IEnumerator Execute(float duration)
+    {
+        yield return new WaitForEndOfFrame();
+    }
+
+    protected override void OnEnd()
+    {
+
+    }
+
+    protected override void OnStart()
+    {
+        CoroutineRunner.instance.StartCoroutine(overflowLiquid.flood(floodTime));
+    }
+}
