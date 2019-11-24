167991fc5d3b66fb805c165e59cbf09dc9157582
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-11-23 17:11:34 -0800

    Make boss hits actually hurt :(

---
+++ SOADsavesMetal/Assets/Scripts/BossAttackHitBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BossAttackHitBox : MonoBehaviour
+{
+    private Collider2D hitbox;
+
+    void Start()
+    {
+        hitbox = GetComponent<Collider2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Player>().Health -= 1;
+        }
+    }
+}
