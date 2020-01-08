383a28698bffa49a89bb0be98899e8313358b296
Oscar Amarillas <oscar16amarillas@gmail.com>
2020-01-08 13:44:56 -0800

    update input manager(work around for canvas interaction) and add pause function and basic menu in pause test scene

---
+++ SOADsavesMetal/Assets/Scripts/GameplayPause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayPause : MonoBehaviour
+{
+    public bool paused = false;
+    public GameObject pauseMenu;
+    private float timeScale; 
+
+    private void Start()
+    {
+        timeScale = Time.timeScale;
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            togglePause();
+        }
+    }
+    public void togglePause()
+    {
+        if(paused)
+        {
+            Play();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+        paused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    private void Play()
+    {
+        Time.timeScale = timeScale;
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
+}
