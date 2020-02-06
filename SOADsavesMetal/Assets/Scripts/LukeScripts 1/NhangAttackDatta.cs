cfe7a3a7bb63c1c1052749c4902118faa1e6430d
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-06 14:19:30 -0800

    I did a lot of background script creation for Nhang(Boss 3) so it works, The boss health is a bit weird there is something wrong in the BossHit script

---
+++ SOADsavesMetal/Assets/Scripts/LukeScripts 1/NhangAttackDatta.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class NhangAttackData
+{
+    public GameObject player;
+    public GameObject agas;
+    public DecayOverflow DecayOverflow;
+    public Animator Splash, Spill;
+    public Transform agasPosition;
+    public Transform attackParent;
+    public CandleEmitter candle0;
+    public CandleEmitter candle1;
+    public CandleEmitter candle2;
+    public CandleEmitter candle3;
+    public CandleEmitter candle4;
+    public CandleEmitter candle5;
+    public CandleEmitter candle6;
+}
