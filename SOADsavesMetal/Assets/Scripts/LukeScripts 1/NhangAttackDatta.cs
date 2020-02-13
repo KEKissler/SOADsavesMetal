604b7d63c5e6c69eb898f3885de3004915b2085a
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-13 15:17:22 -0800

    This is the preliminary work on Boss3's attack stuff

--- SOADsavesMetal/Assets/Scripts/LukeScripts 1/NhangAttackDatta.cs
+++ SOADsavesMetal/Assets/Scripts/LukeScripts 1/NhangAttackDatta.cs
@@ -4,16 +4,8 @@
 public class NhangAttackData
 {
     public GameObject player;
-    public GameObject agas;
-    public DecayOverflow DecayOverflow;
-    public Animator Splash, Spill;
-    public Transform agasPosition;
+    public GameObject Nhang;
+    public Transform SnakePosition;
     public Transform attackParent;
-    public CandleEmitter candle0;
-    public CandleEmitter candle1;
-    public CandleEmitter candle2;
-    public CandleEmitter candle3;
-    public CandleEmitter candle4;
-    public CandleEmitter candle5;
-    public CandleEmitter candle6;
+    public Transform[] tearEmitters;
 }
