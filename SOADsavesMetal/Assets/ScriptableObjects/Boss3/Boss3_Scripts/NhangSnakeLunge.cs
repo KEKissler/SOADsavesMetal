d24cc7959aae02381c4026bea074affd3b7bb2b5
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-18 15:56:22 -0800

    Nhang phase 1 done kinda

---
+++ SOADsavesMetal/Assets/ScriptableObjects/Boss3/Boss3_Scripts/NhangSnakeLunge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Attack/Nhang/Lunge")]
+public class NhangSnakeLunge : NhangAttack
+{
+    GameObject player;
+    GameObject snakeBase;
+
+    public override void Initialize(NhangAttackData data)
+    {
+        player = data.player;
+        snakeBase = data.NhangBase;
+    }
+
+    protected override IEnumerator Execute(float duration)
+    {
+        yield return null;
+        if(snakeBase.GetComponent<Nhang>() != null)
+            snakeBase.GetComponent<Nhang>().StartCoroutine(snakeBase.GetComponent<Nhang>().lunge(player));
+    }
+
+    protected override void OnEnd()
+    {
+
+    }
+
+    protected override void OnStart()
+    {
+        
+    }
+
+    
+}
