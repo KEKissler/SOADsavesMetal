534e9bd7b7b03a9d644839dc55e50dc7960f5b0b
Unknown <chaseoqueso@gmail.com>
2020-02-18 16:00:10 -0800

    H*cking DDR attack boiz

---
+++ SOADsavesMetal/Assets/Scripts/BossAttacks/Boss2/DDRBeam.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Attack/Tsovinar/DDRBeam")]
+public class DDRBeam : TsovinarAttack
+{
+    [System.Serializable]
+    public enum Direction
+    {
+        Right,
+        Up,
+        Left,
+        Down
+    }
+
+    public GameObject ddrbeamPrefab;
+    [SerializeField]
+    private Direction direction;
+
+    private readonly System.Random RNG = new System.Random();
+    private Color color;
+    private GameObject beam;
+    private GameObject[] screens = new GameObject[4];
+    private Transform arrow;
+    private float velocity = -12;
+
+    public override void Initialize(TsovinarAttackData data)
+    {
+        screens = new GameObject[] { data.screen4, data.screen2, data.screen3, data.screen5 };
+    }
+
+    protected override void OnStart()
+    {
+        int dir = RNG.Next(4);
+        beam = Instantiate(ddrbeamPrefab);
+        arrow = beam.transform.GetChild(1);
+        switch (dir)
+        {
+            case 0:
+                direction = Direction.Right;
+                arrow.Rotate(new Vector3(0, 0, 180));
+                break;
+            case 1:
+                direction = Direction.Up;
+                arrow.Rotate(new Vector3(0, 0, -90));
+                break;
+            case 2:
+                direction = Direction.Left;
+                velocity = -16;
+                break;
+            case 3:
+                direction = Direction.Down;
+                arrow.Rotate(new Vector3(0, 0, 90));
+                break;
+            default:
+                Debug.Log("DDRBeam selected a direction that doesn't exist.");
+                break;
+        }
+        
+        arrow.GetComponent<SpriteRenderer>().color = color = screens[dir].GetComponent<SpriteRenderer>().color;
+        beam.GetComponent<DDRBeamManager>().direction = direction;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, 0);
+    }
+
+    protected override IEnumerator Execute(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
---
+++ SOADsavesMetal/Assets/Scripts/BossAttacks/Boss2/DDRBeamManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDRBeamManager : MonoBehaviour
+{
+    public DDRBeam.Direction direction;
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        GameObject player = other.gameObject;
+        if (player.tag == "Player")
+        {
+            switch(direction)
+            {
+                case DDRBeam.Direction.Up:
+                    if (!(player.GetComponent<Rigidbody2D>().velocity.y > 0))
+                        other.gameObject.GetComponent<Player>().Health -= 1;
+                    break;
+                case DDRBeam.Direction.Right:
+                    if(!(player.GetComponent<Rigidbody2D>().velocity.x > 0))
+                        other.gameObject.GetComponent<Player>().Health -= 1;
+                    break;
+                case DDRBeam.Direction.Left:
+                    if (!(player.GetComponent<Rigidbody2D>().velocity.x < 0))
+                        other.gameObject.GetComponent<Player>().Health -= 1;
+                    break;
+                case DDRBeam.Direction.Down:
+                    if (!(player.GetComponent<Rigidbody2D>().velocity.y < 0 || player.GetComponent<Player>().crouched))
+                        other.gameObject.GetComponent<Player>().Health -= 1;
+                    break;
+            }
+        }
+    }
+}
