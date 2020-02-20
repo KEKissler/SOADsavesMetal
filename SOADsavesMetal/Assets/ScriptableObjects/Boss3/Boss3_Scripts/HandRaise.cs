6d198fca7668ade0c47159ce677bd15c942f06dd
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-20 14:42:08 -0800

    Got the health script working for Nhang We will need to do this for boss 4

---
+++ SOADsavesMetal/Assets/ScriptableObjects/Boss3/Boss3_Scripts/HandRaise.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Attack/Nhang/HandRaise")]
+public class HandRaise : NhangAttack
+{
+    GameObject[] HandObjects;
+    GameObject HandObject;
+    int numberOfHands;
+
+    public override void Initialize(NhangAttackData data)
+    {
+        numberOfHands = data.NhangHand.Length;
+        HandObjects = data.NhangHand;
+        HandObject = HandObjects[Random.Range(0, numberOfHands)];
+    }
+
+    protected override IEnumerator Execute(float duration)
+    {
+        bool isUp = false;
+        for(int i = 0; i<4 && !isUp; i++)
+        {
+            HandObject = HandObjects[Random.Range(0, numberOfHands)];
+        }
+        yield return null;
+        if(HandObject.GetComponent<NhangHand>() != null)
+        {
+            HandObject.GetComponent<NhangHand>().moveUp();
+        }
+    }
+
+    protected override void OnEnd()
+    {
+    }
+
+    protected override void OnStart()
+    {
+    }
+
+    
+}
