f65349e0a8d409063bb3099dc9fa8654e59c9748
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-25 14:12:09 -0800

    Start

---
+++ SOADsavesMetal/Assets/ScriptableObjects/Boss3/Boss3_Scripts/PushScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushScript : MonoBehaviour
+{
+    bool Left;
+    Rigidbody2D rb;
+    [SerializeField]
+    float leftPosition;
+    [SerializeField]
+    float rightPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(rb.position.x >= leftPosition)
+        {
+            Left = false;
+        }else
+        {
+            Left = true;
+        }
+        if (Left && rb.position.y < leftPosition)
+        {
+            rb.position = rb.position + new Vector2(.1f,0);
+            
+        }
+        else if (!Left && rb.position.x > rightPosition)
+        {
+            rb.position = rb.position - new Vector2( .1f,0);
+        }
+    }
+}
