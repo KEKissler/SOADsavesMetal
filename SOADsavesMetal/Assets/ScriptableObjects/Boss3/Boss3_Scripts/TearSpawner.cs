d464f46acf12fb2d8351895fbd636e8e65eed014
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-02-25 15:54:35 -0800

    Set up infrastructure for the last push for boss3

--- SOADsavesMetal/Assets/ScriptableObjects/Boss3/Boss3_Scripts/TearSpawner.cs
+++ SOADsavesMetal/Assets/ScriptableObjects/Boss3/Boss3_Scripts/TearSpawner.cs
@@ -8,16 +8,41 @@ public class TearSpawner : MonoBehaviour
     GameObject Tear;
     [SerializeField]
     float[] times;
+    int tear;
+    float time;
+    bool isCrying = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
+        tear = 0;
+    }
+
+    public void Cry()
+    {
+        isCrying = true;
+        time = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isCrying)
+        {
+            if(Time.time - time >= times[tear])
+            {
+                time = Time.time;
+                //Spawn bullet
+                if(tear < times.Length)
+                {
+                    tear++;
+                }
+                else
+                {
+                    tear = 0;
+                }
+            }
+        }
     }
 }
