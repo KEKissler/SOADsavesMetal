321dd762f3b83429a5dca37ec13ed09515cf6bc8
Victoria B <vbarinova97@gmail.com>
2020-03-03 15:35:17 -0800

    Video opening + scene implementation (no skip button yet), minor menu update

---
+++ SOADsavesMetal/Assets/UI Assets/Main Menu/OpeningSequenceManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OpeningSequenceManager : MonoBehaviour
+{
+
+    public VideoPlayer Cutscene;
+    public string levelName;
+    private AsyncOperation synclevel;
+
+    void Start()
+    {
+        //Cutscene.Play(); //Video play must have 'Wait for First Frame" unchecked
+        StartCoroutine("loadLevel");
+
+    }
+    IEnumerator loadLevel()
+    {
+        synclevel = SceneManager.LoadSceneAsync(levelName);
+        synclevel.allowSceneActivation = false;
+        yield return synclevel;
+    }
+
+    void Update()
+    {
+        Cutscene.loopPointReached += OnFinish;
+    }
+
+    private void OnFinish(VideoPlayer vp)
+    {
+        Debug.Log("Cutscene Ended!");
+        //SceneManager.LoadScene(levelName);
+        synclevel.allowSceneActivation = true;
+    }
+
+
+}
