237b0e844ca57557bb6b1545215647dfb09fcb29
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-04-01 15:17:03 -0700

    Stuff in place

---
+++ SOADsavesMetal/Assets/TearShow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TearShow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Tear;
+    [SerializeField]
+    float timeTillDrop = 4;
+    [SerializeField]
+    float flashRate = .2f;
+    float time;
+    float flashTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashTime = 0;
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
