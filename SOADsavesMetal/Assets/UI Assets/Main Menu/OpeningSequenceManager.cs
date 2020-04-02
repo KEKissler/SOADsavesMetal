de86185255c289d512cd24a701508fbeca756842
Victoria B <vbarinova97@gmail.com>
2020-04-01 17:28:14 -0700

    Cutscene Skipping
    + Continue button has a specific color for when it is disabled

--- SOADsavesMetal/Assets/UI Assets/Main Menu/OpeningSequenceManager.cs
+++ SOADsavesMetal/Assets/UI Assets/Main Menu/OpeningSequenceManager.cs
@@ -11,6 +11,8 @@ public class OpeningSequenceManager : MonoBehaviour
     public string levelName;
     private AsyncOperation synclevel;
 
+    public GameObject hint;
+
     void Start()
     {
         //Cutscene.Play(); //Video play must have 'Wait for First Frame" unchecked
@@ -26,14 +28,29 @@ public class OpeningSequenceManager : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Skipping cutscene");
+            OnFinish(Cutscene);
+        }
+
+        if (Input.anyKeyDown) {
+            hint.SetActive(true);
+            Invoke("FadeHint", 3f);
+        }
+
         Cutscene.loopPointReached += OnFinish;
     }
 
     private void OnFinish(VideoPlayer vp)
     {
-        Debug.Log("Cutscene Ended!");
+        //Debug.Log("End of cutscene");
         //SceneManager.LoadScene(levelName);
         synclevel.allowSceneActivation = true;
+    }
+
+    private void FadeHint() {
+        hint.SetActive(false);
     }
 
 
