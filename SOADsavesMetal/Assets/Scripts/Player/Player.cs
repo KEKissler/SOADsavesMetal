9be2eb3788ef99c6947fa08724b21c55276b1856
Keegan Frederick <KDFreder@uci.edu>
2020-04-20 15:42:03 -0700

    Practice room on the mend

--- SOADsavesMetal/Assets/Scripts/Player/Player.cs
+++ SOADsavesMetal/Assets/Scripts/Player/Player.cs
@@ -131,6 +131,11 @@ public class Player : MonoBehaviour
         return currentBandMember + animSuffix;
     }
 
+    public void SetCurrentBandMember(string newBandMember)
+    {
+        currentBandMember = newBandMember;
+    }
+
     public void PlayAudioEvent(string fmodEvent)
     {
         EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
