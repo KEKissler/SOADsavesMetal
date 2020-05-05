782cfa4a4e8d49a6a6385242eab15d5349b73527
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-05-04 19:48:31 -0700

    Work on Boss 4

---
+++ SOADsavesMetal/Assets/SandHandAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandHandAttack : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(collision.gameObject.GetComponent<Player>() != null)
+            {
+                collision.gameObject.GetComponent<Player>().DamagePlayer();
+            }
+        }
+    }
+}
