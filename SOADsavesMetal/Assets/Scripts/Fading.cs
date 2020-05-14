913142af9e5b2b18d6a0fbaf98100b33d535484b
Keegan Frederick <KDFreder@uci.edu>
2020-05-14 13:04:36 -0700

    Expected conflicts in PR

---
+++ SOADsavesMetal/Assets/Scripts/Fading.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fading : MonoBehaviour
+{
+
+    public Image fadeImage;
+    public float fadeStrength;
+    IEnumerator fadeIn()
+    {
+        float alpha = 0f;
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color fadeColor = fadeImage.color;
+            fadeColor.a = alpha;
+
+            while (alpha >= 0f)
+            {
+                fadeColor.a = alpha;
+                fadeImage.color = fadeColor;
+                alpha -= fadeStrength;
+                yield return new WaitForSecondsRealtime(0.01f);
+            }
+        }
+    }
+
+    IEnumerator fadeOut()
+    {
+        float alpha = 0f;
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color fadeColor = fadeImage.color;
+            fadeColor.a = alpha;
+
+            while (alpha <= 1f)
+            {
+                fadeColor.a = alpha;
+                fadeImage.color = fadeColor;
+                alpha += fadeStrength;
+                yield return new WaitForSecondsRealtime(0.01f);
+            }
+        }
+    }
+}
