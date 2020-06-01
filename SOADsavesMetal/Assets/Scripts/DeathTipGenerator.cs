a6e17076d173d065abdfedf6bd75380a000d529f
chaseoqueso <chaseoqueso@gmail.com>
2020-06-01 14:32:23 -0700

    Very beginning of deathtips

---
+++ SOADsavesMetal/Assets/Scripts/DeathTipGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTipGenerator
+{
+    public enum Boss {
+        Agas,
+        Nhang,
+        Tsovinar,
+        Sandaramet
+    }
+
+    public static string GenerateDeathTip(Boss boss, int phase)
+    {
+        
+    }
+
+    private readonly string[][] agasTips = {
+        new string[] {
+            "qwer"
+        }
+    };
+}
