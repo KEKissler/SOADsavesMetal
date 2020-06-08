56798016d3316f18784e8e011214be4bc43a9882
Keegan Frederick <KDFreder@uci.edu>
2020-06-08 14:32:36 -0700

    Removed controller up

--- SOADsavesMetal/Assets/Scripts/Player/PlayerJump.cs
+++ SOADsavesMetal/Assets/Scripts/Player/PlayerJump.cs
@@ -14,7 +14,7 @@ public class PlayerJump : MonoBehaviour
     public void HandleJump()
     {
         if ((ps.currentBandMember == "John" || !ps.isSuperActive) &&
-            (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(ps.GetUp()) || (Input.GetAxis("ContVert") < 0))
+            (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(ps.GetUp()))
                     && ps.remainingJumps > 0)
         {
             tryJump = true;
